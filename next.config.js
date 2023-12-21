ec8bacf54f9b0ef4a07256feb50d0b9539abf84c
Bobbaru <bobbaru_krueger@yahoo.com>
2023-12-21 20:20:55 +0200

    install dependencies and setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // domains: ["https://image.tmdb.org/"],
+    // formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ unlighthouse.config.ts
@@ -0,0 +1,14 @@
+export default {
+  site: "https://next14-movie-hub.vercel.app/",
+  scanner: {
+    // exclude specific routes
+    // exclude: ["/.*?pdf", ".*/amp", "en-*"],
+    // run lighthouse for each URL 3 times
+    samples: 1,
+    // use desktop to scan
+    device: "mobile",
+    // enable the throttling mode
+    throttle: true,
+  },
+  debug: true,
+};
