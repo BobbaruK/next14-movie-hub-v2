386846c88e669db7357a564090b14a24d32b219c
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-05 00:31:44 +0200

    useTransition for movie pagination and filtering by genre

--- src/components/MoviePagination.tsx
+++ src/components/MoviePagination.tsx
@@ -2,6 +2,7 @@ import { MoviesResponse } from "@/types/movies/movie/MoviesResponse";
 import { TVShowsResponse } from "@/types/movies/tv/TVShowsResponse";
 import moviesFetchConfig from "@/utils/moviesFetchConfig";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 interface Props {
   page: number;
@@ -27,38 +28,50 @@ const MoviePagination = ({
 
   const router = useRouter();
 
+  const [isPending, startTransition] = useTransition();
+
   return (
     <div className="flex items-center justify-between gap-4">
       <button
         className="btn btn-primary"
-        disabled={moviesConfig.params.page <= 1}
+        disabled={moviesConfig.params.page <= 1 || isPending}
         onClick={() =>
-          router.push(
-            `?page=${moviesConfig.params.page - 1}${
-              with_genres ? "&with_genres=" + with_genres : ""
-            }${
-              with_original_language
-                ? "&with_original_language=" + with_original_language
-                : ""
-            }${sort_by ? "&sort_by=" + sort_by : ""}`,
+          startTransition(() =>
+            router.push(
+              `?page=${moviesConfig.params.page - 1}${
+                with_genres ? "&with_genres=" + with_genres : ""
+              }${
+                with_original_language
+                  ? "&with_original_language=" + with_original_language
+                  : ""
+              }${sort_by ? "&sort_by=" + sort_by : ""}`,
+            ),
           )
         }
       >
         Prev
       </button>
-      {movie?.page} of {movie?.total_pages} / {movie?.total_results} results
+      {isPending ? (
+        <span className="loading loading-infinity loading-lg"></span>
+      ) : (
+        <>
+          {movie?.page} of {movie?.total_pages} / {movie?.total_results} results
+        </>
+      )}
       <button
         className="btn btn-primary"
-        disabled={moviesConfig.params.page >= movie?.total_pages!}
+        disabled={moviesConfig.params.page >= movie?.total_pages! || isPending}
         onClick={() => {
-          router.push(
-            `?page=${Number(moviesConfig.params.page) + 1}${
-              with_genres ? "&with_genres=" + with_genres : ""
-            }${
-              with_original_language
-                ? "&with_original_language=" + with_original_language
-                : ""
-            }${sort_by ? "&sort_by=" + sort_by : ""}`,
+          startTransition(() =>
+            router.push(
+              `?page=${Number(moviesConfig.params.page) + 1}${
+                with_genres ? "&with_genres=" + with_genres : ""
+              }${
+                with_original_language
+                  ? "&with_original_language=" + with_original_language
+                  : ""
+              }${sort_by ? "&sort_by=" + sort_by : ""}`,
+            ),
           );
         }}
       >
--- src/components/Sidebar/FilteringMovies/ByGenre/ByGenre.tsx
+++ src/components/Sidebar/FilteringMovies/ByGenre/ByGenre.tsx
@@ -4,8 +4,11 @@ import { RQ_MOVIES_GENRES_KEY } from "@/constants";
 import { GenreResponse } from "@/types/movies/GenreResponse";
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useTransition } from "react";
 
 const ByGenre = () => {
+  const [isPending, startTransition] = useTransition();
+
   const { data, error, isLoading } = useQuery<GenreResponse>({
     queryKey: [RQ_MOVIES_GENRES_KEY],
     // queryFn: () => apiClient.getAll(),
@@ -28,12 +31,18 @@ const ByGenre = () => {
 
   return (
     <div className="mb-6">
-      <h3>ByGenre</h3>
+      <h3 className="flex items-center gap-4">
+        ByGenre
+        {isPending && (
+          <span className="loading loading-infinity loading-md"></span>
+        )}
+      </h3>
       <div className="flex flex-wrap gap-2">
         {data?.genres.map((genre) => (
           <button
             className={[
               "badge",
+              "p-3",
               `${
                 genresArr?.includes(String(genre.id))
                   ? "badge-accent"
@@ -42,33 +51,35 @@ const ByGenre = () => {
             ].join(" ")}
             key={genre.id}
             onClick={() => {
-              if (genresArr?.includes(String(genre.id))) {
-                const ind = genresArr.indexOf(String(genre.id));
-                genresArr.splice(ind, 1);
+              startTransition(() => {
+                if (genresArr?.includes(String(genre.id))) {
+                  const ind = genresArr.indexOf(String(genre.id));
+                  genresArr.splice(ind, 1);
+
+                  router.push(
+                    `?page=1${
+                      genresArr.length
+                        ? "&with_genres=" + genresArr.join(",")
+                        : ""
+                    }${
+                      langParams ? "&with_original_language=" + langParams : ""
+                    }${sortByParams ? "&sort_by=" + sortByParams : ""}`,
+                  );
+
+                  return;
+                }
+
+                genresArr?.push(String(genre.id));
 
                 router.push(
-                  `?page=1${
-                    genresArr.length
-                      ? "&with_genres=" + genresArr.join(",")
-                      : ""
-                  }${
+                  `?page=1&with_genres=${genresArr}${
                     langParams ? "&with_original_language=" + langParams : ""
                   }${sortByParams ? "&sort_by=" + sortByParams : ""}`,
                 );
-
-                return;
-              }
-
-              genresArr?.push(String(genre.id));
-
-              router.push(
-                `?page=1&with_genres=${genresArr}${
-                  langParams ? "&with_original_language=" + langParams : ""
-                }${sortByParams ? "&sort_by=" + sortByParams : ""}`,
-              );
+              });
             }}
           >
-            {genre.id} - {genre.name}
+            {genre.name}
           </button>
         ))}
       </div>
