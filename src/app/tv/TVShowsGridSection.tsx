42ee7116f56df333121c4586418a0df44989ed2a
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-05 02:30:06 +0200

    fetch tv shows

---
+++ src/app/tv/TVShowsGridSection.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import MovieGrid from "@/components/MovieGrid";
+import MoviePagination from "@/components/MoviePagination";
+import { RQ_POPULAR_TVSHOWS_ENDPOINT } from "@/constants";
+import MyAPIClient from "@/services/myApiClient";
+import { TVShowsResponse } from "@/types/movies/tv/TVShowsResponse";
+import moviesFetchConfig from "@/utils/moviesFetchConfig";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+
+interface Props {
+  page: number;
+  with_genres: string;
+  sort_by: string;
+  with_original_language: string;
+  queryKey: string;
+}
+
+const TVShowsGridSection = ({
+  page,
+  with_genres,
+  sort_by,
+  with_original_language,
+  queryKey,
+}: Props) => {
+  const moviesConfig = moviesFetchConfig(
+    page,
+    with_genres,
+    with_original_language,
+    sort_by,
+  );
+
+  const apiClient = new MyAPIClient<TVShowsResponse>(
+    RQ_POPULAR_TVSHOWS_ENDPOINT,
+  );
+
+  const { data, error, isLoading } = useQuery<TVShowsResponse>({
+    queryKey: [queryKey, moviesConfig.params],
+    queryFn: () => apiClient.getAll(moviesConfig),
+    placeholderData: keepPreviousData,
+  });
+
+  if (error) return <div className="alert alert-error">{error.message}</div>;
+
+  if (isLoading)
+    return <div className="alert alert-info">Loading TV Shows...</div>;
+
+  if (data?.results.length === 0)
+    return <div className="alert alert-warning">No results</div>;
+
+  return (
+    <>
+      <div className="mb-4">
+        <MoviePagination
+          movie={data}
+          page={page}
+          with_genres={with_genres}
+          sort_by={sort_by}
+          with_original_language={with_original_language}
+        />
+      </div>
+      <MovieGrid movies={data} />
+      <div className="mt-4">
+        <MoviePagination
+          movie={data}
+          page={page}
+          with_genres={with_genres}
+          sort_by={sort_by}
+          with_original_language={with_original_language}
+        />
+      </div>
+    </>
+  );
+};
+
+export default TVShowsGridSection;
--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -1,7 +1,82 @@
-export default function TVShows() {
+import { FilteringMovies } from "@/components/Sidebar/FilteringMovies";
+import {
+  RQ_LANGUAGES_ENDPOINT,
+  RQ_LANGUAGES_KEY,
+  RQ_POPULAR_TVSHOWS_ENDPOINT,
+  RQ_POPULAR_TVSHOWS_KEY,
+  RQ_TVSHOWS_GENRES_ENDPOINT,
+  RQ_TVSHOWS_GENRES_KEY,
+} from "@/constants";
+import MyAPIClient from "@/services/myApiClient";
+import { MovieFilterParams } from "@/types/QueryParams";
+import { GenreResponse } from "@/types/movies/GenreResponse";
+import { Language } from "@/types/movies/Language";
+import { TVShowsResponse } from "@/types/movies/tv/TVShowsResponse";
+import moviesFetchConfig from "@/utils/moviesFetchConfig";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import TVShowsGridSection from "./TVShowsGridSection";
+
+interface Props {
+  searchParams: MovieFilterParams;
+}
+
+export default async function TVShows({
+  searchParams: { page, with_genres, sort_by, with_original_language },
+}: Props) {
+  const pageNumber = parseInt(page);
+  const queryClient = new QueryClient();
+
+  const moviesConfig = moviesFetchConfig(
+    pageNumber,
+    with_genres,
+    with_original_language,
+    sort_by,
+  );
+
+  const apiClientPopularMovies = new MyAPIClient<TVShowsResponse>(
+    RQ_POPULAR_TVSHOWS_ENDPOINT,
+  );
+  await queryClient.prefetchQuery({
+    queryKey: [RQ_POPULAR_TVSHOWS_KEY, moviesConfig.params],
+    queryFn: () => apiClientPopularMovies.getAll(moviesConfig),
+  });
+
+  const apiClientLanguages = new MyAPIClient<Language[]>(RQ_LANGUAGES_ENDPOINT);
+  await queryClient.prefetchQuery({
+    queryKey: [RQ_LANGUAGES_KEY],
+    queryFn: () => apiClientLanguages.getAll(),
+  });
+
+  const apiClientGenres = new MyAPIClient<GenreResponse>(
+    RQ_TVSHOWS_GENRES_ENDPOINT,
+  );
+  await queryClient.prefetchQuery({
+    queryKey: [RQ_TVSHOWS_GENRES_KEY],
+    queryFn: () => apiClientGenres.getAll(),
+  });
+
   return (
-    <div>
-      <h1>Popular TVShows</h1>
-    </div>
+    <>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="appContaier flex flex-col gap-8 lg:flex-row">
+          <div className="lg:basis-1/4">
+            <FilteringMovies genresRQKey={RQ_TVSHOWS_GENRES_KEY} />
+          </div>
+          <div className="lg:basis-3/4">
+            <TVShowsGridSection
+              page={pageNumber}
+              with_genres={with_genres}
+              sort_by={sort_by}
+              with_original_language={with_original_language}
+              queryKey={RQ_POPULAR_TVSHOWS_KEY}
+            />
+          </div>
+        </div>
+      </HydrationBoundary>
+    </>
   );
 }
