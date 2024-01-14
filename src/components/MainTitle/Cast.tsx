c3790575e13d66a51430663ba31801d16df475a4
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-14 17:24:41 +0200

    MainTitle PersonCard refactor and Cast component update

--- src/components/MainTitle/Cast.tsx
+++ src/components/MainTitle/Cast.tsx
@@ -1,11 +1,49 @@
 "use client";
 
 import MyAPIClient from "@/services/myApiClient";
-import { CastAndCrew } from "@/types/movies/CastAndCrew";
+import { CastAndCrew, TheCast } from "@/types/movies/CastAndCrew";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import MainTitlePersonCard from "./PersonCard";
+import { ProfileSizes } from "@/types/imageSizes";
+import imageLink from "@/utils/imageLink";
+import ImageTMDB from "../ImageTMDB";
+
+const MainTitlePersonCard = ({ cast }: { cast: TheCast }) => {
+  return (
+    <div className="card overflow-hidden bg-base-100 shadow-md shadow-primary">
+      <figure>
+        <Link href={`/person/${cast.id}`} className="w-full">
+          <ImageTMDB
+            type="poster"
+            alt={cast.name}
+            src={imageLink<ProfileSizes>(
+              "https://image.tmdb.org/t/p/",
+              "w185",
+              cast.profile_path,
+            )}
+            width={185}
+            height={278}
+          />
+        </Link>
+      </figure>
+      <div className="card-body relative flex justify-between gap-1 p-4">
+        <p>
+          <Link href={`/person/${cast.id}`} className="w-full">
+            {cast.name}
+          </Link>
+        </p>
+        <p>
+          <small>
+            <Link href={`/person/${cast.id}`} className="w-full">
+              {cast.character}
+            </Link>
+          </small>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 interface Props {
   queryKey: string;
