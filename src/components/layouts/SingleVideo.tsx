c677910a9713e141f0638b2c2f40c5f8bc82df78
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-22 18:34:38 +0200

    SingleVideo Component

---
+++ src/components/layouts/SingleVideo.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+const SingleVideo = () => {
+  const { id, ytVideoId } = useParams<{ id: string; ytVideoId: string }>();
+
+  return (
+    <div className="appContaier">
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${ytVideoId}`}
+        title="YouTube video player"
+        // frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        // allowfullscreen
+        className="aspect-video h-auto w-full"
+      ></iframe>
+    </div>
+  );
+};
+
+export default SingleVideo;
