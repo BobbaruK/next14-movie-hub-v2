bf8cdacd9e885fadb36d2b8736afca57af8c278a
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-23 01:49:40 +0200

    delete parallel and intercepting routes from images

--- src/app/movie/[id]/images/backdrops/page.tsx
+++ src/app/movie/[id]/images/backdrops/page.tsx
@@ -1,5 +1,6 @@
+import ImagesGrid from "@/components/layouts/ImagesGrid";
 import MainTitleSidebarLeft from "@/components/layouts/MainTitle/SidebarLeft";
-import Link from "next/link";
+import { RQ_MOVIE_IMAGES_ENDPOINT, RQ_MOVIE_IMAGES_KEY } from "@/constants";
 
 interface Props {
   params: {
@@ -11,20 +12,11 @@ export default function MovieImagesBackdrops({ params: { id } }: Props) {
   return (
     <MainTitleSidebarLeft
       content={
-        <div>
-          <h1>Movie Images Backdrops</h1>
-          <Link href={`/movie/${id}/images/backdrops/12`} scroll={false}>
-            backdrop 12
-          </Link>
-          <br />
-          <Link href={`/movie/${id}/images/backdrops/13`} scroll={false}>
-            backdrop 13
-          </Link>
-          <br />
-          <Link href={`/movie/${id}/images/backdrops/123`} scroll={false}>
-            backdrop 123
-          </Link>
-        </div>
+        <ImagesGrid
+          queryKey={RQ_MOVIE_IMAGES_KEY(id)}
+          endpoint={RQ_MOVIE_IMAGES_ENDPOINT(id)}
+          imagesType="backdrops"
+        />
       }
       sidebar={<p>Main Movie Filtering Sidebar</p>}
     />
