8b29b9ea53eee5585979c05a9b1ca04018a73f90
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-26 21:32:46 +0200

    video grid skeleton update

--- src/components/layouts/VideosGrid.tsx
+++ src/components/layouts/VideosGrid.tsx
@@ -8,8 +8,11 @@ import {
   VideosResponse,
 } from "@/types/VideoResponse";
 import { useQuery } from "@tanstack/react-query";
+import { AlertCircle } from "lucide-react";
 import { useParams } from "next/navigation";
 import VideoCard from "../Cards/Video";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { Skeleton } from "../ui/skeleton";
 
 interface Props {
   queryKey: string;
@@ -37,13 +40,41 @@ const VideosGrid = ({
 
   if (error) throw new Error(`${queryKey} - ${error.message}`);
 
-  if (isLoading)
-    return <div className="alert alert-warning">Loading videos...</div>;
+  if (isLoading) {
+    const CARD_COUNT = 5;
+    const CARDS = Array.from(Array(CARD_COUNT).keys());
+
+    return (
+      <div className="flex flex-col gap-6">
+        <Alert variant="default">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Info</AlertTitle>
+          <AlertDescription>Loading videos...</AlertDescription>
+        </Alert>
+        {CARDS.map((card) => (
+          <div
+            key={card}
+            className="card bg-base-100 flex max-h-56 overflow-hidden shadow-md shadow-primary sm:flex-row"
+          >
+            <Skeleton className="h-[300px] w-[200px] rounded" />
+            <div className="p-3 sm:basis-2/3">
+              <Skeleton className="mb-4 h-[20px] w-[200px] rounded" />
+              <Skeleton className="h-[10px] w-full rounded" />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 
   if (!data?.results.length)
     return (
       <div className="appContaier">
-        <div className="alert alert-warning">No videos</div>
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Info</AlertTitle>
+          <AlertDescription>No videos</AlertDescription>
+        </Alert>
       </div>
     );
 
@@ -54,9 +85,6 @@ const VideosGrid = ({
 
     return theVideoType === videoType;
   });
-
-  if (videos.length === 0)
-    return <div className="alert alert-warning">No videos</div>;
 
   return (
     <div className="flex flex-col gap-6">
