cc0214d60defa027c56d066ea476723bc6642329
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-28 14:20:17 +0200

    main title current season card

--- src/components/MainTitle/tv/CurrentSeason.tsx
+++ src/components/MainTitle/tv/CurrentSeason.tsx
@@ -2,6 +2,14 @@
 
 import Rating from "@/components/Rating";
 import TMDBImages from "@/components/TMDBImages";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import MyAPIClient from "@/services/myApiClient";
 import { TVShowResponse } from "@/types/movies/tv/TVShowResponse";
 import idTitleHyphen from "@/utils/idTitleHyphen";
@@ -41,38 +49,41 @@ const MainTitleCurrentSeason = ({ queryKey, endpoint }: Props) => {
         <div>
           <h2>Current Season</h2>
         </div>
-        <div className="card flex flex-row overflow-hidden bg-base-100 shadow-md shadow-primary">
-          <div className="basis-1/5 [&>img]:h-full [&>img]:w-full [&>img]:object-cover">
-            <Link href={`/tv/${id}/seasons/${lastSeason.season_number}/`}>
-              <TMDBImages
-                type={{ type: "poster", size: "w185" }}
-                alt={lastSeason.name}
-                src={lastSeason.poster_path}
-                width={185}
-                height={278}
-              />
-            </Link>
-          </div>
-          <div className="card-body relative flex flex-shrink basis-4/5 justify-between gap-2 p-4">
-            <div className="flex flex-col">
-              <h3 className="m-0">
+        <Card className="flex flex-col items-center overflow-hidden md:flex-row md:items-stretch">
+          <Link
+            href={`/tv/${id}/seasons/${lastSeason.season_number}/`}
+            className="flex items-center justify-center md:basis-1/5 md:items-start md:justify-start"
+          >
+            <TMDBImages
+              type={{ type: "poster", size: "w185" }}
+              alt={lastSeason.name}
+              src={lastSeason.poster_path}
+              width={185}
+              height={278}
+            />
+          </Link>
+          <div className="flex flex-col justify-between gap-4 md:basis-4/5">
+            <CardHeader className="flex flex-col text-center md:text-start">
+              <CardTitle>
                 <Link href={`/tv/${id}/seasons/${lastSeason.season_number}`}>
                   {lastSeason.name}
                 </Link>
-              </h3>
-              <div className="flex flex-row gap-4">
+              </CardTitle>
+              <CardDescription className="flex flex-row justify-center gap-4 md:justify-start">
                 <Rating vote={lastSeason.vote_average} />
                 <div>{ReleaseDateUI(lastSeason.air_date).year}</div>
                 &bull;
                 <div>{lastSeason.episode_count} Episodes</div>
-              </div>
-            </div>
-            <div>
-              {lastSeason.overview.length < 400
-                ? lastSeason.overview
-                : lastSeason.overview.substring(0, 400) + "..."}
-            </div>
-            <div className="flex items-center gap-2">
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>
+                {lastSeason.overview.length < 400
+                  ? lastSeason.overview
+                  : lastSeason.overview.substring(0, 400) + "..."}
+              </p>
+            </CardContent>
+            <CardFooter className="flex items-center gap-2">
               <FaCalendarAlt />
               <Link
                 href={`/tv/${id}/seasons/${
@@ -87,9 +98,9 @@ const MainTitleCurrentSeason = ({ queryKey, endpoint }: Props) => {
                 {data?.last_episode_to_air.episode_number},{" "}
                 {ReleaseDateUI(data?.last_episode_to_air.air_date).releaseDate})
               </Link>
-            </div>
+            </CardFooter>
           </div>
-        </div>
+        </Card>
       </div>
       <div className="py-4">
         <Link href={`/tv/${id}/seasons`}>View all seasons</Link>
