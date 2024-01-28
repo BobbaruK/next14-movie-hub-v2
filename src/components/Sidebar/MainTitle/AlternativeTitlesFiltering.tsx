df2a0d5a2509a851ef299e19e7cd438bab4af070
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-28 19:46:13 +0200

    alternative titles filtering update

--- src/components/Sidebar/MainTitle/AlternativeTitlesFiltering.tsx
+++ src/components/Sidebar/MainTitle/AlternativeTitlesFiltering.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import { RQ_COUNTRIES_ENDPOINT, RQ_COUNTRIES_KEY } from "@/constants";
 import MyAPIClient from "@/services/myApiClient";
 import { Country } from "@/types/Country";
@@ -54,65 +63,59 @@ const AlternativeTitlesFiltering = ({ title, queryKey, endpoint }: Props) => {
 
   return (
     <>
-      <h2 className="m-0 flex items-center justify-between bg-primary px-2 py-4 text-primary-content">
-        {title}
-        <div
-          className={[
-            "badge",
-            "badge-primary-content",
-            "text-primary",
-            "gap-2",
-            "p-3",
-          ].join(" ")}
-        >
-          {titles.length}
-        </div>
-      </h2>
-      <ul className="flex flex-col gap-1 py-2">
-        {titlesCountries
-          ?.sort((a, b) => {
-            if (a < b) {
-              return -1;
-            }
-            if (a > b) {
-              return 1;
-            }
-            return 0;
-          })
-          .map((country, index) => (
-            <li key={index} className="p-2 hover:bg-slate-600">
-              <Link
-                href={`#${country}`}
-                className={[
-                  "flex",
-                  "items-center",
-                  "justify-between",
-                  "w-full",
-                ].join(" ")}
-              >
-                {
-                  countries?.find((cntry) => cntry.iso_3166_1 === country)
-                    ?.english_name
+      <Card className="overflow-hidden">
+        <h2 className="text-primary-content m-0 flex items-center justify-between bg-primary px-2 py-4">
+          {title}
+          <Badge variant="secondary">{titles.length}</Badge>
+        </h2>
+        <CardContent className="p-0">
+          <ul className="flex flex-col gap-1 py-2">
+            {titlesCountries
+              ?.sort((a, b) => {
+                if (a < b) {
+                  return -1;
                 }
-                <div
-                  className={[
-                    "badge",
-                    "badge-secondary",
-                    "text-secondary-content",
-                    "gap-2",
-                    "p-3",
-                  ].join(" ")}
-                >
-                  {/* {renderedImages.filter((imgs) => imgs.iso_639_1 === lang).length} */}
-                  {
-                    titles.filter((title) => title.iso_3166_1 === country)
-                      .length
-                  }
-                </div>
-              </Link>
-            </li>
-          ))}
-      </ul>
+                if (a > b) {
+                  return 1;
+                }
+                return 0;
+              })
+              .map((country, index) => (
+                <li key={index} className="p-2 hover:bg-slate-600">
+                  <Link
+                    href={`#${country}`}
+                    className={[
+                      "flex",
+                      "items-center",
+                      "justify-between",
+                      "w-full",
+                    ].join(" ")}
+                  >
+                    {
+                      countries?.find((cntry) => cntry.iso_3166_1 === country)
+                        ?.english_name
+                    }
+                    <div
+                      className={[
+                        "badge",
+                        "badge-secondary",
+                        "text-secondary-content",
+                        "gap-2",
+                        "p-3",
+                      ].join(" ")}
+                    >
+                      {/* {renderedImages.filter((imgs) => imgs.iso_639_1 === lang).length} */}
+                      {
+                        titles.filter((title) => title.iso_3166_1 === country)
+                          .length
+                      }
+                    </div>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </CardContent>
+      </Card>
     </>
   );
 };
