f6e52998099ace7cdc94e824167dd1b159dbdc1f
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-28 19:39:11 +0200

    releases filtering update

--- src/components/Sidebar/MainTitle/ReleasesFiltering.tsx
+++ src/components/Sidebar/MainTitle/ReleasesFiltering.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { IsoLang } from "@/components/IsoLang";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
 import MyAPIClient from "@/services/myApiClient";
 import { ReleaseDatesResponse } from "@/types/movies/movie/ReleaseDates";
 import { useQuery } from "@tanstack/react-query";
@@ -26,61 +28,56 @@ const ReleasesFiltering = ({ title, queryKey, endpoint }: Props) => {
 
   return (
     <>
-      <h2 className="m-0 flex items-center justify-between bg-primary px-2 py-4 text-primary-content">
-        {title}
-        <div
-          className={[
-            "badge",
-            "badge-primary-content",
-            "text-primary",
-            "gap-2",
-            "p-3",
-          ].join(" ")}
-        >
-          {data?.results.length}
-        </div>
-      </h2>
-      <ul className="flex flex-col gap-1 py-2">
-        {data?.results
-          .sort((a, b) => {
-            if (a.iso_3166_1 < b.iso_3166_1) {
-              return -1;
-            }
-            if (a.iso_3166_1 > b.iso_3166_1) {
-              return 1;
-            }
-            return 0;
-          })
-          .map((translation, index) => (
-            <li
-              key={translation.iso_3166_1 + index}
-              className="p-2 hover:bg-slate-600"
-            >
-              <Link
-                href={`#${translation.iso_3166_1}`}
-                className={[
-                  "flex",
-                  "items-center",
-                  "justify-between",
-                  "w-full",
-                ].join(" ")}
-              >
-                <IsoLang iso={translation.iso_3166_1} />
-                <div
-                  className={[
-                    "badge",
-                    "badge-secondary",
-                    "text-secondary-content",
-                    "gap-2",
-                    "p-3",
-                  ].join(" ")}
+      <Card className="overflow-hidden">
+        <h2 className="text-primary-content m-0 flex items-center justify-between bg-primary px-2 py-4">
+          {title}
+          <Badge variant="secondary">{data?.results.length}</Badge>
+        </h2>
+        <CardContent className="p-0">
+          {" "}
+          <ul className="flex flex-col gap-1 py-2">
+            {data?.results
+              .sort((a, b) => {
+                if (a.iso_3166_1 < b.iso_3166_1) {
+                  return -1;
+                }
+                if (a.iso_3166_1 > b.iso_3166_1) {
+                  return 1;
+                }
+                return 0;
+              })
+              .map((translation, index) => (
+                <li
+                  key={translation.iso_3166_1 + index}
+                  className="p-2 hover:bg-slate-600"
                 >
-                  {translation.release_dates.length}
-                </div>
-              </Link>
-            </li>
-          ))}
-      </ul>
+                  <Link
+                    href={`#${translation.iso_3166_1}`}
+                    className={[
+                      "flex",
+                      "items-center",
+                      "justify-between",
+                      "w-full",
+                    ].join(" ")}
+                  >
+                    <IsoLang iso={translation.iso_3166_1} />
+                    <div
+                      className={[
+                        "badge",
+                        "badge-secondary",
+                        "text-secondary-content",
+                        "gap-2",
+                        "p-3",
+                      ].join(" ")}
+                    >
+                      {translation.release_dates.length}
+                    </div>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </CardContent>
+      </Card>
     </>
   );
 };
