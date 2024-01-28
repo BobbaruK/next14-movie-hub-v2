e9116d79e15396b99a32bee9c89c292b1e7caa08
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-28 13:22:18 +0200

    review card update

--- src/components/Cards/Review.tsx
+++ src/components/Cards/Review.tsx
@@ -45,7 +45,7 @@ const ReviewCard = ({ review }: Props) => {
     <>
       <Card>
         <CardContent className="flex flex-col justify-between gap-4 p-4">
-          <div className="flex items-center gap-4">
+          <div className="flex flex-col items-center gap-4 sm:flex-row">
             <TMDBImages
               type={{ type: "profile", size: "w45" }}
               alt={review.author}
@@ -55,12 +55,15 @@ const ReviewCard = ({ review }: Props) => {
               className="h-[45px] w-[45px] rounded-full object-cover"
             />
             <div>
-              <h3 className="m-0">A review by {review.author}</h3>
-              <div className="flex items-center gap-4">
+              <h3 className="m-0 text-center sm:text-start">
+                <span className="hidden sm:inline">A review by</span>{" "}
+                {review.author}
+              </h3>
+              <div className="flex flex-col items-center gap-4 sm:flex-row">
                 {review.author_details.rating && (
                   <Rating vote={review.author_details.rating} />
                 )}
-                <div>
+                <div className="text-center sm:text-start">
                   Written by {review.author} on {created.releaseDate}
                 </div>
               </div>
