5f34287150ac5575ba00bc8d5fb26388dccd98b0
Bobbaru <bobbaru_krueger@yahoo.com>
2024-01-30 23:23:52 +0200

    seasons update

--- src/components/layouts/MainTitle/Seasons.tsx
+++ src/components/layouts/MainTitle/Seasons.tsx
@@ -2,6 +2,7 @@
 
 import Rating from "@/components/Rating";
 import TMDBImages from "@/components/TMDBImages";
+import { cn } from "@/lib/utils";
 import MyAPIClient from "@/services/myApiClient";
 import { TVShowResponse } from "@/types/movies/tv/TVShowResponse";
 import ReleaseDateUI from "@/utils/releaseDateUI";
@@ -38,19 +39,31 @@ const Seasons = ({ queryKey, endpoint }: Props) => {
     <div className="flex flex-col gap-8">
       {seasons?.map((season) => (
         <div key={season.id} className="border-b-[1px] border-primary pb-8">
-          <div className="appContaier flex flex-col gap-4 lg:flex-row">
+          <div className="appContaier flex flex-col gap-4 sm:flex-row">
             <div className="flex items-center justify-start sm:basis-[154px]">
-              <div className="inline-block overflow-hidden rounded-md [&>img]:object-cover">
-                <Link href={`/tv/${id}/seasons/${season.season_number}`}>
-                  <TMDBImages
-                    type={{ type: "poster", size: "w154" }}
-                    alt={season.name}
-                    src={season.poster_path}
-                  />
-                </Link>
-              </div>
+              {/* <div className="inline-block overflow-hidden rounded-md [&>img]:object-cover"> */}
+              <Link
+                href={`/tv/${id}/seasons/${season.season_number}`}
+                className="w-full"
+              >
+                <TMDBImages
+                  type={{ type: "poster", size: "w154" }}
+                  alt={season.name}
+                  src={season.poster_path}
+                  className={cn(
+                    `h-seasonPosterImageHeight sm:h-seasonPosterImageHeight-sm w-full overflow-hidden 
+                    rounded-md `,
+                  )}
+                  sizes={`
+                    (max-width: 320px) 300px,
+                    (max-width: 639px) 586px,
+                    154px
+                  `}
+                />
+              </Link>
+              {/* </div> */}
             </div>
-            <div className="flex flex-col justify-center gap-4 py-4 sm:basis-[100%]">
+            <div className="flex flex-col justify-center gap-4 py-4 sm:grow">
               <h2 className="m-0">
                 <Link href={`/tv/${id}/seasons/${season.season_number}`}>
                   {season.name}
