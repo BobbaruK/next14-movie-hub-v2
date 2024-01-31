fc456d52cc9d18adbe36b9d3a9ae800cc15d7b0f
Bobbaru <bobbaru_krueger@yahoo.com>
2024-02-01 00:52:36 +0200

    imgs blur data

--- src/components/TMDBImages.tsx
+++ src/components/TMDBImages.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { cn } from "@/lib/utils";
 import {
   BackdropSizes,
@@ -314,6 +312,21 @@ const TMDBImages = ({
     }
   };
 
+  // Pixel GIF code adapted from https://stackoverflow.com/a/33919020/266535
+  const keyStr =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+  const triplet = (e1: number, e2: number, e3: number) =>
+    keyStr.charAt(e1 >> 2) +
+    keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+    keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+    keyStr.charAt(e3 & 63);
+
+  const rgbDataURL = (r: number, g: number, b: number) =>
+    `data:image/gif;base64,R0lGODlhAQABAPAA${
+      triplet(0, r, g) + triplet(b, 255, 255)
+    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
   return (
     <>
       <div
@@ -331,6 +344,8 @@ const TMDBImages = ({
           fill
           className="object-cover"
           sizes={sizes}
+          placeholder="blur"
+          blurDataURL={rgbDataURL(37, 181, 6)}
         />
       </div>
     </>
