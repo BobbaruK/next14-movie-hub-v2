85f691dfafdf2476756239df1f4fc31eef21ee62
Bobbaru <bobbaru_krueger@yahoo.com>
2024-03-04 00:06:23 +0200

    trending section lazy loading

--- src/components/Home/TrendingSection/index.tsx
+++ src/components/Home/TrendingSection/index.tsx
@@ -1,8 +1,29 @@
+"use client";
+
+import Spinner from "@/components/Spinner";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import TrendingDay from "./TrendingDay";
-import TrendingWeek from "./TrendingWeek";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const DynamicTrendingDay = dynamic(() => import("./TrendingDay"), {
+  loading: () => (
+    <p className="flex items-center justify-start gap-4">
+      <Spinner /> Loading day trending...
+    </p>
+  ),
+});
+
+const DynamicTrendingWeek = dynamic(() => import("./TrendingWeek"), {
+  loading: () => (
+    <p className="flex items-center justify-start gap-4">
+      <Spinner /> Loading week trending...
+    </p>
+  ),
+});
 
 const TrendingSection = () => {
+  const [showWeekTrending, setShowWeekTrending] = useState<boolean>(false);
+
   return (
     <div className="container">
       <Tabs defaultValue="day" className="flex flex-col gap-4">
@@ -10,15 +31,15 @@ const TrendingSection = () => {
           <h2 className="m-0 font-bold">Trending</h2>
           <TabsList className="">
             <TabsTrigger value="day">Today</TabsTrigger>
-            <TabsTrigger value="week">This Week</TabsTrigger>
+            <TabsTrigger value="week" onClick={()=>setShowWeekTrending(true)}>This Week</TabsTrigger>
           </TabsList>
         </div>
         <div>
           <TabsContent value="day" className="m-0">
-            <TrendingDay />
+            <DynamicTrendingDay />
           </TabsContent>
           <TabsContent value="week" className="m-0">
-            <TrendingWeek />
+            {showWeekTrending && <DynamicTrendingWeek />}
           </TabsContent>
         </div>
       </Tabs>
